7757a61bf4319df615633c29f6cfe85ab9d2c89e
Jasberraja <jasberraja@gmail.com>
2020-11-14 20:54:48 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { JassComponent } from "./jass/jass.component";
-
+import { Jass2Component } from "./jass2/jass2.component";
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, JassComponent],
+  declarations: [AppComponent, JassComponent, Jass2Component],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/jass2/jass2.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-jass2",
+  templateUrl: "./jass2.component.html"
+})
+export class Jass2Component {}
+// own component by jasber
